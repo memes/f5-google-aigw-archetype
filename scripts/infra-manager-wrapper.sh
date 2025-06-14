29bacb1e6996b61821c8cb36653cbaab77232edb
Matthew Emes <memes@matthewemes.com>
2025-06-13 19:08:50 -0700

    ci: Infra-manager, without nesting envs

--- scripts/infra-manager-wrapper.sh
+++ scripts/infra-manager-wrapper.sh
@@ -6,19 +6,21 @@ set -e
 
 plan()
 {
-    [ -z "${PREVIEW_NAME}" ] && echo "ERROR: PREVIEW_NAME environment variable must be set" && exit 1
-    [ -z "${DEPLOYMENT_NAME}" ] && echo "ERROR: DEPLOYMENT_NAME environment variable must be set" && exit 1
+    [ -z "${DEPLOYMENT_ID}" ] && echo "ERROR: DEPLOYMENT_ID environment variable must be set" && exit 1
+    [ -z "${DEPLOYMENT_PROJECT_ID}" ] && echo "ERROR: DEPLOYMENT_PROJECT_ID environment variable must be set" && exit 1
+    [ -z "${DEPLOYMENT_REGION}" ] && echo "ERROR: DEPLOYMENT_REGION environment variable must be set" && exit 1
     [ -z "${IAC_SERVICE_ACCOUNT_NAME}" ] && echo "ERROR: IAC_SERVICE_ACCOUNT_NAME environment variable must be set" && exit 1
     [ -z "${GITHUB_URL}" ] && echo "ERROR: GITHUB_URL environment variable must be set" && exit 1
     [ -z "${GITHUB_SHA}" ] && echo "ERROR: GITHUB_SHA environment variable must be set" && exit 1
     [ -z "${SOURCE_DIRECTORY}" ] && echo "ERROR: SOURCE_DIRECTORY environment variable must be set" && exit 1
-    [ -z "${TF_VERSION}" ] && echo "ERROR: TF_VERSION environment variable must be set" && exit 1
 
     # Delete existing preview, if it exists
+    PREVIEW_NAME="projects/${DEPLOYMENT_PROJECT_ID}/locations/${DEPLOYMENT_REGION}/previews/${GITHUB_SHA}"
     gcloud infra-manager previews delete --quiet "${PREVIEW_NAME}" 2>/dev/null || true
 
     # See if there is an existing deployment to attach to the preview
-    EXISTING_DEPLOYMENT="$(gcloud infra-manager deployments describe "${DEPLOYMENT_NAME}" --format "value(name)" 2>/dev/null || true)"
+    DEPLOYMENT_NAME="projects/${DEPLOYMENT_PROJECT_ID}/locations/${DEPLOYMENT_REGION}/deployments/${DEPLOYMENT_ID}"
+    HAS_EXISTING_DEPLOYMENT="$(gcloud infra-manager deployments describe "${DEPLOYMENT_NAME}" --format "value(name)" 2>/dev/null || true)"
 
     # Create a new preview for the commit
     gcloud infra-manager previews create "${PREVIEW_NAME}" \
@@ -27,7 +29,7 @@ plan()
         --git-source-ref "${GITHUB_SHA}" \
         --git-source-directory "${SOURCE_DIRECTORY}" \
         --inputs-file "${GITHUB_SHA}.tfvars" \
-        ${EXISTING_DEPLOYMENT:+--deployment="${DEPLOYMENT_NAME}"}
+        ${HAS_EXISTING_DEPLOYMENT:+--deployment "${DEPLOYMENT_NAME}"}
 
     # Export the tfplan from preview
     gcloud infra-manager previews export "${PREVIEW_NAME}" --file "${GITHUB_SHA}"
@@ -39,12 +41,16 @@ plan()
 
 apply()
 {
-    [ -z "${DEPLOYMENT_NAME}" ] && echo "ERROR: DEPLOYMENT_NAME environment variable must be set" && exit 1
+    [ -z "${DEPLOYMENT_ID}" ] && echo "ERROR: DEPLOYMENT_ID environment variable must be set" && exit 1
+    [ -z "${DEPLOYMENT_PROJECT_ID}" ] && echo "ERROR: DEPLOYMENT_PROJECT_ID environment variable must be set" && exit 1
+    [ -z "${DEPLOYMENT_REGION}" ] && echo "ERROR: DEPLOYMENT_REGION environment variable must be set" && exit 1
     [ -z "${IAC_SERVICE_ACCOUNT_NAME}" ] && echo "ERROR: IAC_SERVICE_ACCOUNT_NAME environment variable must be set" && exit 1
     [ -z "${GITHUB_URL}" ] && echo "ERROR: GITHUB_URL environment variable must be set" && exit 1
     [ -z "${GITHUB_SHA}" ] && echo "ERROR: GITHUB_SHA environment variable must be set" && exit 1
     [ -z "${SOURCE_DIRECTORY}" ] && echo "ERROR: SOURCE_DIRECTORY environment variable must be set" && exit 1
     [ -z "${TF_VERSION}" ] && echo "ERROR: TF_VERSION environment variable must be set" && exit 1
+
+    DEPLOYMENT_NAME="projects/${DEPLOYMENT_PROJECT_ID}/locations/${DEPLOYMENT_REGION}/deployments/${DEPLOYMENT_ID}"
     gcloud infra-manager deployments apply "${DEPLOYMENT_NAME}" \
         --service-account "${IAC_SERVICE_ACCOUNT_NAME}" \
         --git-source-repo "${GITHUB_URL}" \
@@ -56,8 +62,10 @@ apply()
 
 delete()
 {
-    [ -z "${DEPLOYMENT_NAME}" ] && echo "ERROR: DEPLOYMENT_NAME environment variable must be set" && exit 1
-    gcloud infra-manager deployments delete "${DEPLOYMENT_NAME}"
+    [ -z "${DEPLOYMENT_ID}" ] && echo "ERROR: DEPLOYMENT_ID environment variable must be set" && exit 1
+    [ -z "${DEPLOYMENT_PROJECT_ID}" ] && echo "ERROR: DEPLOYMENT_PROJECT_ID environment variable must be set" && exit 1
+    [ -z "${DEPLOYMENT_REGION}" ] && echo "ERROR: DEPLOYMENT_REGION environment variable must be set" && exit 1
+    gcloud infra-manager deployments delete "projects/${DEPLOYMENT_PROJECT_ID}/locations/${DEPLOYMENT_REGION}/deployments/${DEPLOYMENT_ID}"
 }
 
 case "$1" in
